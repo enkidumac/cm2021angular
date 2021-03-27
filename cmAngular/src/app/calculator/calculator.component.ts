a16e9a2dda550d62c3fd6c394430f4704f76ad8b
TomaszNakonieczny80 <best.o@op.pl>
2021-03-27 09:41:57 +0100

    calculator

--- cmAngular/src/app/calculator/calculator.component.ts
+++ cmAngular/src/app/calculator/calculator.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  value1: number = 0;
-  value2: number = 0;
+  value1?: number;
+  value2?: number;
   result: number = 0;
 
     constructor() { }
@@ -17,7 +17,35 @@ export class CalculatorComponent implements OnInit {
   }
 
   plus(){
+   if(this.value1 &&  this.value2){
+
     this.result = this.value1 + this.value2;
+
+   }
+  }
+
+  minus(){
+    if(this.value1 &&  this.value2){
+
+      this.result = this.value1 - this.value2;
+
+     }
+  }
+
+  multiplic(){
+    if(this.value1 &&  this.value2){
+
+      this.result = this.value1 * this.value2;
+
+     }
+  }
+
+  division(){
+    if(this.value1 &&  this.value2){
+
+      this.result = this.value1 / this.value2;
+
+     }
   }
 
 }
